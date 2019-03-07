0c62be87450d03658240dae2b91932cc4ba069d2
ahoeworx <46934665+ahoeworx@users.noreply.github.com>
2019-03-07 17:46:11 -0500

    Add files via upload

--- SDFunctions.ino
+++ SDFunctions.ino
@@ -3,20 +3,23 @@ void setupSD()
   //Initialize the SD Card Slot
   radio.print("Initializing SD card...");
   pinMode(chipSelect, OUTPUT); //For the SD Card to work
-  if (!SD.begin(chipSelect))
+  if (SD.begin(chipSelect))
   {
-    radio.println("initialization failed!");
+    radio.println("initialization successful!");
+    DCAWS_GPS = SD.open("DCAWS_GPS.csv", FILE_WRITE);
+  }
+  else
+  {
+    Serial.println("initialization failed!");
     return;
   }
-                                  //  DCAWS_Depth = SD.open("DCAWS_Depth.csv", FILE_WRITE);
-  DCAWS_GPS = SD.open("DCAWS_GPS.csv", FILE_WRITE);
 }
 
 void checkSD()
 {
-  if (!SD.begin(chipSelect))
+  if (!DCAWS_GPS)
   {
-    radio.println(F("SD card initialization failed!"));
+    radio.println(F("SD card failed!"));
  //   missionReady = false;
     return;
   }
